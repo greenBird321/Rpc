3677987eac013593dcda7c0f96395af1796e741d
2hehe <1395992734@qq.com>
2020-07-28 19:50:45 +0800

    更新由于没有消耗，或者获得钻石的用户数据结构问题

--- app/Services/ML_lmyz/UserService.php
+++ app/Services/ML_lmyz/UserService.php
@@ -155,20 +155,24 @@ WHERE
             $result['user_id']         = $value['RoleID'];                                          // role_id
             $result['server']          = $value['ServerId'];                                        // 服务器id
             $result['name']            = $value['name'];                                            // role_name
-            $result['money_type'][]    = $value['ItemType'];                                        // 钱类型
-            $result['money_num'][]     = $value['ItemNum'];                                         // 钱
+            if (!empty($value['ItemType'])) {
+                $result['money_type'][] = $value['ItemType'];                                        // 钱类型
+            }
+            if (!empty($value['ItemNum'])) {
+                $result['money_num'][]     = $value['ItemNum'];                                         // 钱
+            }
             $result['level']           = $value['PlayerLv'];                                        // 等级
             $result['exp']             = $value['playerExp'];                                       // 经验值
             $result['morale']          = $value['morale'];                                          // 士气
             $result['ArenaPoint']      = $value['ArenaPoint'];                                      // 高阶竞技场积分
-            $result['ArenaWinTime']    = date("Y-m-d H:i:s",$value['ArenaWinTime']);         // 竞技场挑战时间（时间戳）
+            $result['ArenaWinTime']    = date("Y-m-d H:i:s",$value['ArenaWinTime']);                // 竞技场挑战时间（时间戳）
             $result['RobValue']        = $value['RobValue'];                                        // 掠夺值
             $result['GoodValue']       = $value['GoodValue'];                                       // 善良值
-            $result['LogoutTime']      = date('Y-m-d H:i:s', $value['LogoutTime']);          // 登出时间
+            $result['LogoutTime']      = date('Y-m-d H:i:s', $value['LogoutTime']);                 // 登出时间
             $result['MPower']          = $value['MPower'];                                          // 战力
             $result['WandLevel']       = $value['WandLevel'];                                       // 鼓舞系统星级
             $result['device_id']       = $value['DeviceID'];                                        // 设备id
-            $result['create_time']     = date('Y-m-d H:i:s', $value['CreateTime']);          // 角色创建时间
+            $result['create_time']     = date('Y-m-d H:i:s', $value['CreateTime']);                 // 角色创建时间
             $result['active_day']      = $value['ActiveDay'];                                       // 活跃天数
             $result['ban']             = $value['ban'];                                             // 账号封禁
             $result['quick_afkNum']    = $value['QuickAFKNum'];                                     // 今日快速挂机次数
